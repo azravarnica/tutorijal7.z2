49f8ea898f34bae52caa2af081f48a2f1b149f37
azravarnica <avarnica1@etf.unsa.ba>
2019-05-06 12:08:09 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.function.Function;
+
+public class Main {
+    public static void naRaspon100(Function<Double, Double> func) {
+        for(double i=0; i<100; i++) {
+            System.out.println(func.apply(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        naRaspon100((Double x) -> x*x+2*x+1);
+
+
+    }
+    public static void main(String [] args) {
+        Integer brojevi[] = new Integer [] {
+                11,12,41,12,243,42,123
+        } ;
+        Arrays.sort(brojevi, (x,y) -> sum(x) - sum(y));
+        brojevi = Arrays.stream(brojevi).
+                boxed().
+                sorted((x, y) -> sum(x)- sum(y)).
+                mapToInt(i -> i).
+                toArray();
+    }
+    private static int sum(int x ) {
+        return x;
+    }
+}
